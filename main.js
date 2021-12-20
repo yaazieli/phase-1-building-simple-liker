069557b38eed3f20bbfb3b4221b39c41d929855d
Yaazieli <yaazieli@gmail.com>
2021-12-19 19:50:44 -0500

    commit tree a18852ebc88daa026b28dd62ae9af1ed8d7092d6

--- main.js
+++ main.js
@@ -4,8 +4,32 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const articleHearts = document.querySelectorAll(".like-glyph");
+function likeCallback(e) {
+  const heart = e.target;
+  
+mimicServerCall("bogusUrl")
+.then(function(){
+  if ( heart.innerText === EMPTY_HEART) {
+    heart.innerText = FULL_HEART;
+    heart.className = "activated-heart";
+  } else {
+    heart.innerText = EMPTY_HEART;
+    heart.className = "";
+  }
+})
+.catch(function(error) {
+  const modal = document.getElementById("modal");
+  modal.className = "";
+  modal.innerText = error;
+  setTimeout(() =>  modal.className = "hidden", 3000);
+});
+}
 
 
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
